808eb1296d7eb5e36a4feb3c26c1fc261dfcc92e
Dixon <59415488+radixon@users.noreply.github.com>
2022-04-30 13:43:04 -0400

    Create AlgerianForestFires.py
    # Data Set Information:
    
    The dataset includes 244 instances that regroup a data of two regions of Algeria,namely the Bejaia region located in the northeast of Algeria and the Sidi Bel-abbes region located in the northwest of Algeria.
    
    122 instances for each region.

---
+++ AlgerianForestFires.py
@@ -0,0 +1,5 @@
+# Import files from UCI Machine Learning Repository
+import pandas as pd
+path = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00547/Algerian_forest_fires_dataset_UPDATE.csv'
+df = pd.read_csv(path)
+print(data.iloc[:5])
